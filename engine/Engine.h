2d543e4548085e4ec3c0174e86f66f514521ef41
Maria9950 <m.aulowa@yangex.ru>
2025-12-01 09:26:07 +0300

    Add command engine

--- engine/Engine.h
+++ engine/Engine.h
@@ -1,1 +1,31 @@
 #pragma once
+
+#include <string>
+#include <unordered_map>
+#include <functional>
+#include <stdexcept>
+
+#include "wrapper/Wrapper.h"
+
+class Engine {
+public:
+    Engine();
+
+    template<typename Class, typename... Args>
+    void registerCommand(const std::string& name,
+        Wrapper<Class, Args...>* wrapper) {
+        if (commands_.find(name) != commands_.end()) {
+            throw std::runtime_error("Engine: command already registered: " + name);
+        }
+
+        commands_[name] = [wrapper](const ArgsMap& args) -> int {
+            return wrapper->execute(args);
+            };
+    }
+
+    int execute(const std::string& name, const ArgsMap& args) const;
+
+private:
+    using CommandFunc = std::function<int(const ArgsMap&)>;
+    std::unordered_map<std::string, CommandFunc> commands_;
+};
