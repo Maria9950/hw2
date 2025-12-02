0c29fcc270a7d2f8c6d237b1c22a20ce02a4788c
Maria9950 <m.aulowa@yangex.ru>
2025-12-02 08:14:54 +0300

    Add math module with basic operations

--- modules/MathModule.cpp
+++ modules/MathModule.cpp
@@ -1,1 +1,17 @@
 #include "modules/MathModule.h"
+
+int MathModule::add(int a, int b) {
+    return a + b;
+}
+
+int MathModule::sub(int a, int b) {
+    return a - b;
+}
+
+int MathModule::mul(int a, int b) {
+    return a * b;
+}
+
+int MathModule::linear(int a, int b, int c) {
+    return a * b + c;
+}
